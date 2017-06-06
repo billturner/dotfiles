a5cd4567b477bb92aa9358fab02a5a3261bbe4b0
Bill Turner <billturner@gmail.com>
2017-06-05 22:26:52 -0400

    reorganization of emacs

---
+++ emacs.d/init.el
@@ -0,0 +1,16 @@
+;; init.el
+
+;; package management
+(require 'package)
+(add-to-list 'package-archives
+	     '("melpa" . "http://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	     '("org" . "http://orgmode.org/elpa/") t)
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(eval-when-compile
+  (require 'use-package))
+(require 'bind-key)
