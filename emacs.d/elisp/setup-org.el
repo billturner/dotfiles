38fd67b20795c5b6aea932cdb0d354b88959154d
Bill Turner <billturner@gmail.com>
2018-01-24 15:59:15 -0500

    a few org changes

--- emacs.d/elisp/setup-org.el
+++ emacs.d/elisp/setup-org.el
@@ -3,6 +3,7 @@
 ;; org vars
 (setq my-org-directory "~/src/org")
 (setq my-default-org-file "~/src/org/organizer.org")
+(setq my-choice-point-org-file "~/src/org/choice-points.org")
 (setq my-work-org-file "~/src/org/work.org")
 (setq my-work-devlog-org-file "~/src/org/devlog-work.org")
 
@@ -11,6 +12,9 @@
                 (lambda () (interactive) (find-file my-default-org-file)))
 (global-set-key (kbd "C-c w")
                 (lambda () (interactive) (find-file my-work-org-file)))
+(global-set-key (kbd "C-c t")
+                (lambda () (interactive) (find-file my-choice-point-org-file)))
+
 (use-package org
   :ensure t
   :mode ("\\.org\\'" . org-mode)
@@ -26,25 +30,48 @@
     (setq org-log-done 'time)
     (setq org-capture-templates
           '(("t" "Todo" entry (file+headline my-default-org-file "Inbox")
-             "* TODO %^{Task}\n\n")
+             "* TODO %^{Task}\n")
             ("i" "Idea" entry (file+headline my-default-org-file "Ideas")
-             "* %^{Idea} / %t\n\n")
-            ("w" "Work Todo" entry (file+headline my-work-org-file "Inbox")
-             "* TODO %^{Task}\n\n")
+             "* %^{Idea} / %t\n")
+             ("w" "Work Todo" entry (file+headline my-work-org-file "Inbox")
+             "* TODO %^{Task}\n")
             ("l" "Work Devlog" entry (file+datetree my-work-devlog-org-file)
-             "* %^{Summary}\n\n")
+             "* %^{Summary}\n%?")
+            ("c" "CP: Towards" entry (file+datetree my-choice-point-org-file)
+             "* TOWARDS %^{Action}\n")
+            ("a" "CP: Away" entry (file+datetree my-choice-point-org-file)
+             "* AWAY %^{Action}\n")
             )
           )
     (setq org-todo-keywords
           '(
-            (sequence "TODO(t)" "NEXT(n)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")))
+            (sequence "TODO(t)" "NEXT(n)" "WAITING(w)" "|" "DONE(d)" "CANCELLED(c)")
+            (sequence "TOWARDS(o)" "AWAY(a)")
+            ))
     (setq org-todo-keyword-faces
           '(("TODO" . (:foreground "orangered" :weight bold))
             ("NEXT" . (:foreground "steelblue1" :weight bold))
             ("WAITING" . (:foreground "lightgoldenrod" :weight bold))
             ("DONE" . (:foreground "forestgreen" :weight bold))
             ("CANCELLED" . (:foreground "forestgreen" :weight bold))
+            ("TOWARDS" . (:foreground "forestgreen" :weight bold))
+            ("AWAY" . (:foreground "orangered" :weight bold))
             ))
+    (setq org-tag-alist
+          '(
+            (:startgroup . nil)
+            ("@personal" . ?p)
+            ("@work" . ?w)
+            ("@project" . ?j)
+            ("@blog" . ?b)
+            (:endgroup . nil)
+            )
+          )
+    (setq org-tag-faces
+          '(
+            ("@personal" . (:foreground "forestgreen" :weight bold))
+            )
+          )
     ))
 
 (provide 'setup-org)
